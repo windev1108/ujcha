143469385e81be031545046753af7b63633eb0d7
Win1 <vuhuynh@var-meta.com>
2026-06-26 21:29:30 +0700

    fix: update prisma

--- apps/api/prisma/migrations/20260626042307_add_platform_display_options/migration.sql
+++ apps/api/prisma/migrations/20260626042307_add_platform_display_options/migration.sql
@@ -1,3 +1,4 @@
 -- AlterTable
-ALTER TABLE "DeliveryPlatform" ADD COLUMN     "displayMode" TEXT NOT NULL DEFAULT 'logo_and_text',
-ADD COLUMN     "logoSize" TEXT NOT NULL DEFAULT 'md';
+ALTER TABLE "DeliveryPlatform" ADD COLUMN "displayMode" TEXT NOT NULL DEFAULT 'logo_and_text',
+ALTER TABLE "DeliveryPlatform" ADD COLUMN "logoWidth" INTEGER NOT NULL DEFAULT 28;
+ALTER TABLE "DeliveryPlatform" ADD COLUMN "logoHeight" INTEGER NOT NULL DEFAULT 28;
